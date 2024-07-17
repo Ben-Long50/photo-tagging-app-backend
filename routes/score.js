e461983c66ceacff44164c88f6c2c3585e8626cb
Ben-Long50 <benjlong50@gmail.com>
2024-07-16 17:41:04 -0700

    deleted uneccessary files

--- routes/score.js
+++ routes/score.js
@@ -1,12 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 import scoreController from '../controllers/scoreController.js';
+import authController from '../controllers/authController.js';
 
 const router = express.Router();
 
-router.get('/levels/scores', scoreController.getAllScores);
+router.get('/levels/scores', cors(), scoreController.getAllScores);
 
-router.get('/levels/:levelId/scores', scoreController.getLevelScores);
+router.get('/levels/:levelId/scores', cors(), scoreController.getLevelScores);
 
-router.post('/levels/:levelId/scores', scoreController.createScore);
+router.post(
+  '/levels/:levelId/scores',
+  cors(),
+  authController.verifyToken,
+  scoreController.createScore,
+);
 
 export default router;
